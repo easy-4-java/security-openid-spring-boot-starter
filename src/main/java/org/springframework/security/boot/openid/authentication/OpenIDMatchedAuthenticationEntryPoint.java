121549131f24620c4a89f14b933afe402f032aaf
wandl <hiwepy@163.com>
2026-05-27 11:14:14 +0800

    chore(pom): align to Spring Boot 3.3.x (JDK 17, Boot 3.3.13)

--- src/main/java/org/springframework/security/boot/openid/authentication/OpenIDMatchedAuthenticationEntryPoint.java
+++ src/main/java/org/springframework/security/boot/openid/authentication/OpenIDMatchedAuthenticationEntryPoint.java
@@ -17,9 +17,9 @@ package org.springframework.security.boot.openid.authentication;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationEntryPoint;
 import org.springframework.security.core.AuthenticationException;
