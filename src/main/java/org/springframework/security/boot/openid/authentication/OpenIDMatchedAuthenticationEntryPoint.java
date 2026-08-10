d22071156f66a4e90e8f3403008c8582a1278848
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:00:56 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/openid/authentication/OpenIDMatchedAuthenticationEntryPoint.java
+++ src/main/java/org/springframework/security/boot/openid/authentication/OpenIDMatchedAuthenticationEntryPoint.java
@@ -26,7 +26,7 @@ import org.springframework.security.core.AuthenticationException;
 
 /**
  * TODO
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class OpenIDMatchedAuthenticationEntryPoint implements MatchedAuthenticationEntryPoint {
 
